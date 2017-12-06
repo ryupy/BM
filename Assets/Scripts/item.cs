c76a89de484197f32465825ea061ed4b0935c413
Douhaku.R <ryuuji@christina.local>
2017-12-06 10:22:24 +0900

    171206

--- Assets/Scripts/item.cs
+++ Assets/Scripts/item.cs
@@ -2,22 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class item : Token {
+public class item : MonoBehaviour {
 
-	public bool itemex = true;
-
+	public Canvas canvasitemget = null;
+	
 	// Use this for initialization
 	void Start () {
-		
+		if (canvasitemget != null) {
+			canvasitemget.enabled = false;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (canvasitemget.enabled = true) {
+			if (Input.GetKey (KeyCode.DownArrow)) {
+				if (canvasitemget != null) {
+					canvasitemget.enabled = false;
+				}
+			}
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D collision){
-		DestroyObj();
-		itemex = false;
+		if (collision.tag == "Player") {
+			Destroy (this.gameObject);
+			itemget ();
+		}
+	}
+
+	public void itemget(){
+		if (canvasitemget != null) {
+			canvasitemget.enabled = true;
+		}
 	}
 }
