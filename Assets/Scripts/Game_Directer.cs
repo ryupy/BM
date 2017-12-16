a38ee36dbcf7c639de1bae2928606532fb80d6cf
Douhaku.R <ryuuji@christina.local>
2017-12-16 23:46:26 +0900

    Add Audio

--- Assets/Scripts/Game_Directer.cs
+++ Assets/Scripts/Game_Directer.cs
@@ -6,43 +6,45 @@ public class Game_Directer : MonoBehaviour {
 
 
 	public Canvas canvasitemget = null;
+	public Canvas canvasitemmenu = null;
 	public bool popup = false;
 	private bool popup_now = false;
 	// Use this for initialization
 	void Start () {
-		itemwinreverse ();
+		item_get_win_reverse ();
+		item_menu_win_reverse ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 		if (popup == true) {
-			itemwinreverse ();
+			item_get_win_reverse ();
 			popup = false;
 			popup_now = true;
 			Time.timeScale = 0;
 		} else if (Input.GetKey (KeyCode.DownArrow) && popup_now == true) {
 			Debug.Log ("del");
-			itemwinreverse ();
+			item_get_win_reverse ();
 			popup_now = false;
 			Time.timeScale = 1;
 		}
-	
-
-		if (item.item_fish_number == 1) {
-			itemwinreverse ();
-			item.item_fish_number += 1;
-		} 
-		else if (Input.GetKey (KeyCode.DownArrow) && item.item_fish_number == 2) {
-			itemwinreverse ();
-			item.item_fish_number += 1;
-		}
 	}
 
-	public void itemwinreverse(){
+//	public void Button_MenuDown(){
+//		item_menu_win_reverse ();
+//	}
+
+
+	public void item_get_win_reverse(){
 		if (canvasitemget != null) {
 			canvasitemget.enabled = !canvasitemget.enabled;
 		}
 	}
 
+	public void item_menu_win_reverse(){
+		if (canvasitemmenu != null) {
+			canvasitemmenu.enabled = !canvasitemmenu.enabled;
+		}
+	}
 }
